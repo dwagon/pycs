0fe53fd21a10912611d2deb57f09ee5a83b485bf
Dougal Scott <dougal.scott@gmail.com>
2021-08-07 11:11:48 +1000

    Get HP correct

--- pycs/character/fighter.py
+++ pycs/character/fighter.py
@@ -9,9 +9,17 @@ class Fighter(Character):
     """Fighter class"""
 
     def __init__(self, **kwargs):
-        self.hp = 10
         kwargs.update(
-            {"str": 16, "dex": 14, "con": 15, "int": 11, "wis": 13, "cha": 9, "ac": 18}
+            {
+                "str": 16,
+                "dex": 14,
+                "con": 15,
+                "int": 11,
+                "wis": 13,
+                "cha": 9,
+                "ac": 18,
+                "hp": 10,
+            }
         )
         super().__init__(**kwargs)
         self.add_action(MeleeAttack("longsword", reach=5, bonus="+5", dmg="1d8+3"))
