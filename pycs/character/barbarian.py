19bb281183cb8fe078ae164434d9400c9ce9f19c
Dougal Scott <dougal.scott@gmail.com>
2021-08-11 21:30:38 +1000

    Implemented barbarian

---
+++ pycs/character/barbarian.py
@@ -0,0 +1,55 @@
+""" Barbarian """
+import colors
+from attacks import MeleeAttack
+from attacks import RangedAttack
+from constants import DamageType
+from .character import Character
+
+
+##############################################################################
+class Barbarian(Character):
+    """Barbarian class"""
+
+    def __init__(self, **kwargs):
+        kwargs.update(
+            {
+                "str": 16,
+                "dex": 14,
+                "con": 15,
+                "int": 11,
+                "wis": 13,
+                "cha": 9,
+                "ac": 16,
+                "hp": 14,
+            }
+        )
+        super().__init__(**kwargs)
+        self.add_action(
+            MeleeAttack(
+                "Greataxe",
+                reach=5,
+                bonus=5,
+                dmg=("1d12", 3),
+                dmg_type=DamageType.SLASHING,
+            )
+        )
+        self.add_action(
+            RangedAttack(
+                "Javelin",
+                reach=5,
+                bonus=5,
+                ammo=2,
+                dmg=("1d6", 3),
+                dmg_type=DamageType.PIERCING,
+            )
+        )
+
+    def shortrepr(self):
+        """What a fighter looks like in the arena"""
+        if self.is_alive():
+            return colors.blue("B", bg="green")
+        else:
+            return colors.blue("B", bg="red")
+
+
+# EOF
