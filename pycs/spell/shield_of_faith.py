c7195038caa901bbca2ac771f8d2157b12641f3c
Dougal Scott <dougal.scott@gmail.com>
2021-08-18 09:20:29 +1000

    Implement Shield of Faith

--- pycs/spell/shield_of_faith.py
+++ pycs/spell/shield_of_faith.py
@@ -1,5 +1,6 @@
 """https://www.dndbeyond.com/spells/shield-of-faith"""
 
+from effect import Effect
 import spells
 from constants import SpellType
 
@@ -16,6 +17,7 @@ class Shield_Of_Faith(spells.SpellAction):
         kwargs.update(
             {
                 "casting": "bonus",
+                "concentration": True,
                 "reach": 60,
                 "level": 1,
                 "type": SpellType.BUFF,
@@ -26,12 +28,50 @@ class Shield_Of_Faith(spells.SpellAction):
     ###########################################################################
     def cast(self, caster):
         """Do the spell"""
+        friend = self.pick_target(caster)
+        if caster.distance(friend) > self.range()[0]:
+            return False
+        print(f"{caster} casts Shield of Faith on {friend}")
+        friend.add_effect(Shield_Of_Faith_Effect(cause=caster))
         return True
+
+    ###########################################################################
+    def pick_target(self, doer):
+        """Who gets the spell - person with the lowest AC"""
+        targets = []
+        for friend in doer.arena.my_side(doer.side):
+            if friend.has_effect("Shield of Faith"):
+                continue
+            targets.append((friend.ac, id(friend), friend))
+        targets.sort()
+        if not targets:
+            return None
+        return targets[0][-1]
 
     ###########################################################################
     def heuristic(self, doer):
         """Should we do the spell"""
+        if self.pick_target(doer):
+            return 1
         return 0
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+class Shield_Of_Faith_Effect(Effect):
+    """Effect of the Shield of Faith spell"""
+
+    ###########################################################################
+    def __init__(self, **kwargs):
+        """Initialise"""
+        super().__init__("Shield of Faith", **kwargs)
+
+    ###########################################################################
+    def hook_ac_modifier(self, target):
+        eff = super().hook_ac_modifier(target)
+        eff.update({"bonus": 2})
+        return eff
 
 
 # EOF
