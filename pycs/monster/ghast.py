3af84a2fe0edd83299d4b44e396107b1c89b3eb9
Dougal Scott <dougal.scott@gmail.com>
2021-08-18 21:35:33 +1000

    Adapt to new side effect args

--- pycs/monster/ghast.py
+++ pycs/monster/ghast.py
@@ -73,7 +73,7 @@ class Ghast(Monster):
                 print(f"{creat} resists Ghast's poisonous stench")
 
     ##########################################################################
-    def ghast_claws(self, source):  # pylint: disable=unused-argument
+    def ghast_claws(self, source, target):  # pylint: disable=unused-argument
         """If the target is a creature other than an undead, it must
         succeed on a DC 10 Constitution saving throw or be paralyzed for 1
         minute. The target can repeat the saving throw at the end of each
