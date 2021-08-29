7703479ff6960e982687ec43a89bf4ceab58e41a
Dougal Scott <dougal.scott@gmail.com>
2021-08-29 14:47:25 +1000

    Make spell interface consistent

--- pycs/spells/__init__.py
+++ pycs/spells/__init__.py
@@ -1,25 +1,57 @@
 """ Spells """
+from .aid import Aid
+from .absorb_elements import Absorb_Elements
+from .beacon_of_hope import Beacon_Of_Hope
+from .bless import Bless
 from .burning_hands import Burning_Hands
 from .command import Command
+from .cure_wounds import Cure_Wounds
 from .eldritch_blast import Eldritch_Blast
+from .enhance_ability import Enhance_Ability
 from .fireball import Fireball
 from .frostbite import Frostbite
+from .guiding_bolt import Guiding_Bolt
+from .healing_word import Healing_Word
 from .hellish_rebuke import Hellish_Rebuke
+from .hold_person import Hold_Person
+from .hunters_mark import Hunters_Mark
+from .lesser_restoration import Lesser_Restoration
+from .mass_healing_word import Mass_Healing_Word
 from .poison_spray import Poison_Spray
+from .sacred_flame import Sacred_Flame
 from .scorching_ray import Scorching_Ray
 from .shatter import Shatter
+from .shield_of_faith import Shield_Of_Faith
+from .spirit_guardians import Spirit_Guardians
+from .spiritual_weapon import Spiritual_Weapon
 from .thunderclap import Thunderclap
 
 __all__ = [
+    "Aid",
+    "Absorb_Elements",
+    "Beacon_Of_Hope",
+    "Bless",
     "Burning_Hands",
     "Command",
+    "Cure_Wounds",
     "Eldritch_Blast",
+    "Enhance_Ability",
     "Fireball",
     "Frostbite",
     "Frostbite",
+    "Guiding_Bolt",
+    "Healing_Word",
     "Hellish_Rebuke",
+    "Hold_Person",
+    "Hunters_Mark",
+    "Lesser_Restoration",
+    "Mass_Healing_Word",
     "Poison_Spray",
+    "Sacred_Flame",
     "Scorching_Ray",
     "Shatter",
+    "Shield_Of_Faith",
+    "Spirit_Guardians",
+    "Spiritual_Weapon",
     "Thunderclap",
 ]
