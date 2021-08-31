1773841ece34d2a7ababc600366b609a6f21d970
Dougal Scott <dougal.scott@gmail.com>
2021-08-31 20:16:09 +1000

    Implement Burning Hands

---
+++ pycs/spells/burning_hands.py
@@ -0,0 +1,41 @@
+"""https://www.dndbeyond.com/spells/burning-hands"""
+
+from pycs.spell import AttackSpell
+from pycs.constant import DamageType
+from pycs.constant import SpellType
+from pycs.constant import Stat
+
+
+##############################################################################
+class Burning_Hands(AttackSpell):
+    """As you hold your hands with thumbs touching and fingers spread,
+    a thin sheet of flames shoots forth from your outstretched fingertips.
+    Each creature in a 15-foot cone must make a Dexterity saving throw.
+    A creature takes 3d6 fire damage on a failed save, or half as much
+    damage on a successful one.
+
+    The fire ignites any flammable objects in the area that aren't being
+    worn or carried.
+
+    At Higher Levels. When you cast this spell using a spell slot of
+    2nd level or higher, the damage increases by 1d6 for each slot level
+    above 1st."""
+
+    ##########################################################################
+    def __init__(self, **kwargs):
+        name = "Burning Hands"
+        kwargs.update(
+            {
+                "reach": 15,
+                "level": 1,
+                "dmg": ("3d6", 0),
+                "dmg_type": DamageType.FIRE,
+                "style": SpellType.SAVE_HALF,
+                "type": SpellType.RANGED,
+                "save_stat": Stat.DEX,
+            }
+        )
+        super().__init__(name, **kwargs)
+
+
+# EOF
