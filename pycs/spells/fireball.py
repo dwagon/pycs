21b657562034b80ffc26e31a1b2632863fd98028
Dougal Scott <dougal.scott@intelematics.com>
2021-09-07 12:33:30 +1000

    Test fireballs go bang

--- pycs/spells/fireball.py
+++ pycs/spells/fireball.py
@@ -1,11 +1,15 @@
 """https://www.dndbeyond.com/spells/fireball"""
 
 from collections import defaultdict
+from unittest.mock import patch
 import dice
 from pycs.spell import AttackSpell
+from pycs.creature import Creature
+from pycs.constant import ActionCategory
 from pycs.constant import DamageType
 from pycs.constant import SpellType
 from pycs.constant import Stat
+from .spelltest import SpellTest
 
 
 ##############################################################################
@@ -67,25 +71,59 @@ class Fireball(AttackSpell):
                 (sides[enemy.side], 999 - sides[doer.side], id(enemy), enemy)
             )
         # We want the most enemy and the least friends in the blast
-        targets.sort()
         if targets:
             self._num_enemy = targets[0][0]
             self._num_friend = -targets[0][1] + 999
             self.target = targets[0][-1]
+            targets.sort()
             return targets[0][-1]
         return None
 
     ##########################################################################
     def heuristic(self, doer):
         """Should we do the spell"""
-        if not doer.spell_available(self):
-            return 0
         if self.pick_target(doer):
-            return (
+            val = (
                 int(dice.roll_max("8d6")) * self._num_enemy
-                - int(dice.roll_max("8d6")) * self._num_friend
+                - int(1.5 * int(dice.roll_max("8d6"))) * self._num_friend
             )
+            return val
         return 0
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+class TestFireball(SpellTest):
+    """Test Spell"""
+
+    ##########################################################################
+    def setUp(self):
+        super().setUp()
+        self.caster.add_action(Fireball())
+
+    ##########################################################################
+    def test_cast_saved(self):
+        """test casting where victim makes saving throw"""
+        self.assertEqual(self.enemy.hp, self.enemy.max_hp)
+        with patch.object(dice, "roll") as mock_dice:
+            mock_dice.return_value = 20
+            with patch.object(Creature, "rolld20") as mock:
+                mock.return_value = 19
+                self.caster.do_stuff(categ=ActionCategory.ACTION, moveto=False)
+        # Damage should be half of (20 + 4(prof_bonus))
+        self.assertEqual(self.enemy.hp, self.enemy.max_hp - 12)
+
+    ##########################################################################
+    def test_cast_hit(self):
+        """test casting where victim fails saving throw"""
+        self.assertEqual(self.enemy.hp, self.enemy.max_hp)
+        with patch.object(dice, "roll") as mock_dice:
+            mock_dice.return_value = 20
+            with patch.object(Creature, "rolld20") as mock:
+                mock.return_value = 1
+                self.caster.do_stuff(categ=ActionCategory.ACTION, moveto=False)
+        self.assertEqual(self.enemy.hp, self.enemy.max_hp - 24)
 
 
 # EOF
