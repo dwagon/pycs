7d9e27094220275af465acf3fb96fc90eb72601b
Dougal Scott <dougal.scott@gmail.com>
2021-09-15 09:25:34 +1000

    Check arguments of creatures

--- pycs/character.py
+++ pycs/character.py
@@ -2,6 +2,7 @@
 from pycs.creature import Creature
 from pycs.spell import SpellAction
 from pycs.races import Human
+from pycs.util import check_args
 
 
 ##############################################################################
@@ -9,14 +10,19 @@ class Character(Creature):
     """Base character class"""
 
     def __init__(self, **kwargs):
+        check_args(self.valid_args(), self.__class__.__name__, kwargs)
         self.level = kwargs.get("level", 1)
         self.race = kwargs.get("race", Human)()
         self.race.owner = self
         if "prof_bonus" not in kwargs:
             profb = int((kwargs.get("level", 1) - 1) / 4) + 2
             kwargs.update({"prof_bonus": profb})
-        kwargs["effects"] = self.race.effects
         super().__init__(**kwargs)
+
+    ##########################################################################
+    def valid_args(self):
+        """What is valid in this class for kwargs"""
+        return super().valid_args() | {"level", "race"}
 
     ##########################################################################
     def shortrepr(self):
--- pycs/util.py
+++ pycs/util.py
@@ -4,7 +4,7 @@
 def check_args(valid, cname, kwargs):
     """Check that the kwargs are in the permitted list"""
     for arg in kwargs:
-        assert arg in valid, f"{cname}: {arg} not in {valid}"
+        assert arg in valid, f"{cname}: '{arg}' not in {valid}"
 
 
 # EOF
