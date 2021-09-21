6f6186998ff138edcea558eafbdbc40e65b1638b
Dougal Scott <dougal.scott@gmail.com>
2021-09-22 08:16:06 +1000

    Implemented Spirit Guardians

--- pycs/spells/spirit_guardians.py
+++ pycs/spells/spirit_guardians.py
@@ -1,13 +1,17 @@
 """https://www.dndbeyond.com/spells/spirit-guardians"""
 
+import dice
+from pycs.constant import DamageType
 from pycs.constant import SpellType
-from pycs.spell import AttackSpell
+from pycs.constant import Stat
+from pycs.effect import Effect
+from pycs.spell import SpellAction
 
 
 ##############################################################################
 ##############################################################################
 ##############################################################################
-class Spirit_Guardians(AttackSpell):
+class Spirit_Guardians(SpellAction):
     """You call forth spirits to protect you. They flit around you to
     a distance of 15 feet for the duration. If you are good or neutral,
     their spectral form appears angelic or fey (your choice). If you
@@ -29,7 +33,6 @@ class Spirit_Guardians(AttackSpell):
             {
                 "reach": 15,
                 "level": 3,
-                "style": SpellType.SAVE_HALF,
                 "type": SpellType.RANGED,
                 "concentration": SpellType.CONCENTRATION,
             }
@@ -40,9 +43,13 @@ class Spirit_Guardians(AttackSpell):
     def heuristic(self):
         """Should we do the spell
         the more people it can effect the more we should do it"""
-        if not self.owner.spell_available(self):
+        if self.owner.has_effect("Spirit Guardian"):
             return 0
-        return 0
+        heur = 0
+        for enem in self.owner.pick_closest_enemy():
+            if self.owner.distance(enem) < self.range()[0]:
+                heur += 24  # 3d8
+        return heur * 2
 
     ##########################################################################
     def pick_target(self):
@@ -52,11 +59,46 @@ class Spirit_Guardians(AttackSpell):
     ##########################################################################
     def cast(self):
         """Do the spell"""
+        self.owner.add_effect(SpiritGuardianEffect(caster=self.owner))
         return True
 
     ##########################################################################
     def end_concentration(self):
         """What happens when we stop concentrating"""
+        self.owner.remove_effect("Spirit Guardian")
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+class SpiritGuardianEffect(Effect):
+    """Spirit Guardian Effect"""
+
+    ##########################################################################
+    def __init__(self, **kwargs):
+        """Initialise"""
+        super().__init__("Spirit Guardian", **kwargs)
+        self._victims = set()
+
+    ##########################################################################
+    def hook_start_turn(self):
+        """Reset who has been affected this turn"""
+        self._victims = set()
+
+    ##########################################################################
+    def hook_start_in_range(self, creat):
+        """Are we in range of the effect"""
+        if creat.side == self.caster.side:
+            return
+        if self.caster.distance(creat) > 15 / 5:
+            return
+        if creat in self._victims:
+            return
+        self._victims.add(creat)
+        dmg = int(dice.roll("3d8"))
+        if creat.saving_throw(Stat.WIS, self.caster.spellcast_save):
+            dmg /= 2
+        creat.hit(dmg, DamageType.RADIANT, self.caster, False, "Spirit Guardian")
 
 
 # EOF
