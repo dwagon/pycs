65e4bee308891649e89d946c01b21354901bb237
Dougal Scott <Dougal.Scott@gmail.com>
2021-10-20 16:41:39 +1100

    Implement Ankylosaurus

---
+++ pycs/monsters/ankylosaurus.py
@@ -0,0 +1,75 @@
+""" Ankylosaurus  MM p79 """
+import unittest
+import colors
+from pycs.arena import Arena
+from pycs.attack import MeleeAttack
+from pycs.constant import MonsterType, DamageType, Condition, Stat
+from pycs.monster import Monster
+
+
+##############################################################################
+class Ankylosaurus(Monster):
+    """Ankylosaurus"""
+
+    ##########################################################################
+    def __init__(self, **kwargs):
+        kwargs.update(
+            {
+                "hitdice": "8d12+16",
+                "speed": 30,
+                "type": MonsterType.BEAST,
+                "ac": 15,
+                "str": 19,
+                "dex": 11,
+                "con": 15,
+                "int": 2,
+                "wis": 12,
+                "cha": 5,
+                "actions": [
+                    MeleeAttack(
+                        "Tail",
+                        reach=10,
+                        dmg=("4d6", 0),
+                        dmg_type=DamageType.BLUDGEONING,
+                        side_effect=self.anktail,
+                    ),
+                ],
+            }
+        )
+        super().__init__(**kwargs)
+
+    ##########################################################################
+    def anktail(self, source, target, dmg):  # pylint: disable=unused-argument
+        """Ank Tail - must succeed at a DC14 strength save or be knocked prone"""
+        svth = target.saving_throw(Stat.STR, 14)
+        if not svth:
+            target.add_condition(Condition.PRONE)
+
+    ##########################################################################
+    def shortrepr(self):
+        """What a allosaurus looks like on the arena"""
+        if self.is_alive():
+            return colors.blue("K")
+        return colors.blue("K", bg="red")
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+class TestAllosaurus(unittest.TestCase):
+    """Test Ankylosaurus"""
+
+    ##########################################################################
+    def setUp(self):
+        """Set up the lair"""
+        self.arena = Arena()
+        self.beast = Ankylosaurus(side="a")
+        self.arena.add_combatant(self.beast, coords=(5, 5))
+
+    ##########################################################################
+    def test_ac(self):
+        """Test that the AC exists"""
+        self.assertEqual(self.beast.ac, 15)
+
+
+# EOF
