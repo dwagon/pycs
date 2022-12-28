bb4b04ac88ba2d747a5623d801a5846064e98373
Dougal Scott <dougal.scott@gmail.com>
2022-12-28 13:22:52 +1100

    Black'd

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -12,17 +12,9 @@ from pycs.damage import Damage
 ##############################################################################
 class TestEffects(unittest.TestCase):
     """Tests for `effects` class."""
+
     def setUp(self) -> None:
-        self.kwargs = {
-            "str": 6,
-            "int": 7,
-            "dex": 8,
-            "wis": 18,
-            "con": 11,
-            "cha": 15,
-            "side": "a",
-            "hp": 30,
-            "ac": 11}
+        self.kwargs = {"str": 6, "int": 7, "dex": 8, "wis": 18, "con": 11, "cha": 15, "side": "a", "hp": 30, "ac": 11}
         self.owner = Creature(**self.kwargs)
         self.eff = Effects(owner=self.owner)
 
